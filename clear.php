1798e5d1a76ec2e05682842bdce74327fe4dd233
Patrick Cunningham <patrick@indulgemedia.com>
2023-02-24 10:58:27 +0000

    added clear script

---
+++ clear.php
@@ -0,0 +1,16 @@
+<?php
+// Specify the cache directory
+$cache_dir = './cache/';
+
+// Get all the cache files
+$cache_files = glob($cache_dir . '*');
+
+// Loop through the cache files and delete them
+foreach ($cache_files as $cache_file) {
+    if (is_file($cache_file)) {
+        unlink($cache_file);
+    }
+}
+
+// Return a message indicating that the cache has been cleared
+echo 'Cache cleared';
